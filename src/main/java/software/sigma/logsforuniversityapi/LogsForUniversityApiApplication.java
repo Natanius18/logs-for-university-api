2194f554ee1c5fb87e2b9513990bcd75c4e839fb
natan.chachko <natan.chachko@sigma.software>
2022-09-20 15:05:23 +0300

    Initial commit

---
+++ src/main/java/software/sigma/logsforuniversityapi/LogsForUniversityApiApplication.java
@@ -0,0 +1,13 @@
+package software.sigma.logsforuniversityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsForUniversityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsForUniversityApiApplication.class, args);
+    }
+
+}
